ae5cb35000540cee44f61719ee3c0679b2294a10
Ubuntu <ubuntu@ip-172-31-25-249.ap-southeast-2.compute.internal>
2014-08-16 06:08:08 +0000

    Adding MySQL connection script and variables, removed test.txt

---
+++ partB/connect.php
@@ -0,0 +1,16 @@
+<?php
+	require_once('db.php');
+	
+	if(!$dbconn = mysql_connect(DB_HOST, DB_USER, DB_PW))
+	{
+		echo 'Could not connect to mysql on ' . DB_HOST . '\n';
+		exit;
+	}
+
+	if(!mysql_select_db(DB_NAME, $dbconn))
+	{
+		echo 'Could not user database ' . DB_NAME . '\n';
+		echo mysql_error() . '\n';
+		exit;
+	}
+?>
