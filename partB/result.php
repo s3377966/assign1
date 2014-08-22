7a5582e4705f02ea1873a9a85e07d413ae09c6a9
Ubuntu <ubuntu@ip-172-31-25-249.ap-southeast-2.compute.internal>
2014-08-22 06:50:41 +0000

    result page now gets wine id, wine name, year, winery name, region name for all wines

--- partB/result.php
+++ partB/result.php
@@ -1,1 +1,17 @@
+<?php
+	include 'connect.php';
 
+	// Getting the id, name, year, winery name and region name for each wine
+	$winesQuery = "SELECT wine_id, wine_name, year, winery_name, region_name 
+				   FROM wine 
+				   LEFT OUTER JOIN winery ON winery.winery_id = wine.winery_id 
+				   LEFT OUTER JOIN region ON winery.region_id = region.region_id 
+				   GROUP BY wine_id";
+
+	$winesResult = mysql_query($winesQuery);
+
+	while ($row = mysql_fetch_row($winesResult))
+	{
+		echo '<p>'.$row[0].' '.$row[1].' '.$row[2].' '.$row[3].' '.$row[4].'</p>';
+	}
+?>
