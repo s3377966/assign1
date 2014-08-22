5a87f42736ad9e169c0c8a16b225ea2bf0a1fecf
Ubuntu <ubuntu@ip-172-31-25-249.ap-southeast-2.compute.internal>
2014-08-22 09:33:37 +0000

    Added a default 'select region' message to dropdownlist

--- partB/search.php
+++ partB/search.php
@@ -5,7 +5,9 @@
 
 <form action="result.php" method="GET" >
 	<select name="region" onchange="this.form.submit();">
+	<option selected disabled hidden value=''>Select A Region</option>
 		<?php
+			// Adding each region to the dropdownlist
 			while ($row = mysql_fetch_row($regions))
 			{
 				$regionId = $row[0];
