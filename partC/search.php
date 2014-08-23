82048732cfd9f7d0cc76095e7e218b5788670484
Ubuntu <ubuntu@ip-172-31-25-249.ap-southeast-2.compute.internal>
2014-08-23 06:24:15 +0000

    search.php now strictly php code using templator

--- partC/search.php
+++ partC/search.php
@@ -1,19 +1,28 @@
 <?php
+	require_once("MiniTemplator.class.php");
 	include 'connect.php';
-	$regions = mysql_query("SELECT region_id, region_name FROM region");
-?>
 
-<form action="result.php" method="GET" >
-	<select name="region" onchange="this.form.submit();">
-	<option selected disabled hidden value=''>Select A Region</option>
-		<?php
-			// Adding each region to the dropdownlist
-			while ($row = mysql_fetch_row($regions))
-			{
-				$regionId = $row[0];
-				$regionName = $row[1];
-				echo '<option value ="'.$regionId.'">'.$regionName.'</option>';
-			}
-		?>
-	</select>
-</form>
+	$t = new MiniTemplator;
+	$ok = $t->readTemplateFromFile("search.htm");
+
+	// Checking for successfully loading template
+	if (!$ok)
+	{
+		die ("MiniTemplator failed to read search.htm");
+	}
+
+	$regions = mysql_query("SELECT region_id, region_name FROM region");
+
+	// Adding each region to a block to be put in dropdownlist
+	while ($row = mysql_fetch_row($regions))
+	{
+		$regionId = $row[0];
+		$regionName = $row[1];
+
+		$t->setVariable("regionId", $regionId);
+		$t->setVariable("regionName", $regionName);
+		$t->addBlock("region");
+	}
+
+	$t->generateOutput();
+?>
